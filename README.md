e912babfcff7f9a527c038a0a8bc6c11124620a5
Shan <70942213+ShanAZIZ@users.noreply.github.com>
2021-10-09 00:42:44 +0200

    Build: Commit Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Malloc World README
+*Groupe 5 - AL2*
+## Description 
+## Installation - Compilation
+## Fichiers
+
+
+
---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+ * FILENAME: main.c
+ * Made by: GROUPE 5 - AL2 
+ * Projet: Malloc-World
+ * Date de création: 09/10/2021
+ * Dernière modification : 09/10/2021
+ * Par: AZIZ Shana
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}

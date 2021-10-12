852b3f1ad5826404f0d161c01903329054af23f2
Audrey <audrey.divito@protonmail.com>
2021-10-12 16:41:22 +0200

    [Feature] Begin the creation of a map (dynamic)
    Functions to create a map in dynamic.
    TODO : Place the player in the map and the monsters

--- src/main.c
+++ src/main.c
@@ -3,15 +3,17 @@
  * Made by: GROUPE 5 - AL2 
  * Projet: Malloc-World
  * Date de création: 09/10/2021
- * Dernière modification : 09/10/2021
+ * Dernière modification : 12/10/2021
  * Par: AZIZ Shana
  */
 
 #include<stdio.h>
 #include<stdlib.h>
+#include "map/map.h"
 
 int main(int argc, char const *argv[])
 {
-    
+    int** map = createMap(10, 10);
+    displayMap(map, 10, 10);
     return 0;
 }
---
+++ src/map/map.h
@@ -0,0 +1,17 @@
+/**
+ * FILENAME: map.h
+ * Made by: GROUPE 5 - AL2
+ * Projet: Malloc-World
+ * Date de création: 12/10/2021
+ * Dernière modification : 12/10/2021
+ * Par: DI VITO Audrey
+ */
+
+#ifndef MAP_H
+#define MAP_H
+
+int** createMap(int, int);
+void displayMap(int**, int, int);
+int defineRandom(int);
+
+#endif

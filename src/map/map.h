beda65975686ba977d8f24b4653f26aa2e48952a
Audrey <audrey.divito@protonmail.com>
2021-10-13 14:59:50 +0200

    [Feature] Portal placement by zone
    Portal placement according to the placement of the player

--- src/map/map.h
+++ src/map/map.h
@@ -3,16 +3,19 @@
  * Made by: GROUPE 5 - AL2
  * Projet: Malloc-World
  * Date de création: 12/10/2021
- * Dernière modification : 12/10/2021
+ * Dernière modification : 13/10/2021
  * Par: DI VITO Audrey
  */
 
 #ifndef MAP_H
 #define MAP_H
 
-int** createMap(int, int);
+int** createMap(int, int, int);
 void displayMap(int**, int, int);
-int defineRandom(int);
+int defineRandom(int, int);
 int** placePlayer(int**);
+int** generateByZone(int**, int, int, int);
+int** placePortalZone2(int**);
+int** placePortalZone3(int**);
 
 #endif

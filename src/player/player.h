143a4f844a450d0d05e22ea93c07b1670c72c22e
Shan <70942213+ShanAZIZ@users.noreply.github.com>
2021-10-15 23:17:07 +0200

    build : Implementation de la structure player et fonction initplayer

---
+++ src/player/player.h
@@ -0,0 +1,36 @@
+#ifndef PLAYER
+#define PLAYER
+#endif
+
+#define INVENTORY_LENGHT 10
+
+// Structures
+/**
+ * Struct Player
+ * TODO: Ecrire la docString
+ * TODO: Le joueur ne pourra avoir que 3 armes
+ * TODO: Le joueur ne pourra avoir que 1 armure
+ * TODO: Les ressources du craft peuvent s'empiler
+ * 
+ */
+typedef struct Player
+{
+    int xp;
+    int level;
+    int maxHp;
+    int currentHp;
+    int inventory[INVENTORY_LENGHT];
+}Player;
+
+
+// Fonctions
+/**
+ * A utiliser en debut de partie
+ * permet d'initialiser un joueur avec les informations minimums 
+ */
+Player* initPlayer();
+void appendWeaponToInventory(int stuffId);
+void appendArmorToInventory(int stuffId);
+void appendResourceToInventory(int stufftId);
+void appendToInventory(int stuffId); // Utilisera les 3 fonctions ci dessus selon le stuffId
+

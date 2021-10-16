f88beaf9d1ed391877c237c228b8be3ff590e096
Shan <70942213+ShanAZIZ@users.noreply.github.com>
2021-10-16 19:08:23 +0200

    [Fix] cleanup du code et écriture des docstring

--- src/player/player.h
+++ src/player/player.h
@@ -2,35 +2,32 @@
 #define PLAYER
 #endif
 
-#define INVENTORY_LENGHT 10
+#define INVENTORY_TOTAL 10
 
-// Structures
 /**
  * Struct Player
- * TODO: Ecrire la docString
  * TODO: Le joueur ne pourra avoir que 3 armes
  * TODO: Le joueur ne pourra avoir que 1 armure
  * TODO: Les ressources du craft peuvent s'empiler
- * 
  */
-typedef struct Player
-{
+typedef struct Player{
     int xp;
     int level;
     int maxHp;
     int currentHp;
-    int inventory[INVENTORY_LENGHT];
-}Player;
+    int inventory[INVENTORY_TOTAL]; // Sera au definitif un array de struct "item"
+}player;
 
-
-// Fonctions
 /**
- * A utiliser en debut de partie
- * permet d'initialiser un joueur avec les informations minimums 
+ * Initialise un joueur de debut de partie
+ * @return
  */
-Player* initPlayer();
-void appendWeaponToInventory(int stuffId);
-void appendArmorToInventory(int stuffId);
-void appendResourceToInventory(int stufftId);
-void appendToInventory(int stuffId); // Utilisera les 3 fonctions ci dessus selon le stuffId
+player* initPlayer();
+
+/**
+ * Ajoute un objet/arme/ressource dans l'inventaire d'un joueur
+ * @param stuff_id
+ * @param player
+ */
+void appendToInventory(int stuff_id, player* player);
 

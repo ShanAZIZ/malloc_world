ee2ee202d43bcfd15427063e70807262f3172021
Armand <armanddailly@gmail.com>
2021-10-17 00:15:53 +0200

    [Init] Init monsters branch

---
+++ src/monsters/monsters.c
@@ -0,0 +1,10 @@
+/**
+ * FILENAME: monsters.c
+ * Made by: GROUPE 5 - AL2
+ * Projet: Malloc-World
+ * Date de création: 17/10/2021
+ * Dernière modification : 17/10/2021
+ * Par: DAILLY Armand
+**/
+
+#include "monsters.h"
---
+++ src/monsters/monsters.h
@@ -0,0 +1,13 @@
+/**
+ * FILENAME: monsters.h
+ * Made by: GROUPE 5 - AL2
+ * Projet: Malloc-World
+ * Date de création: 17/10/2021
+ * Dernière modification : 17/10/2021
+ * Par: DAILLY Armand
+**/
+
+#ifndef MALLOC_WORLD_MONSTERS_H
+#define MALLOC_WORLD_MONSTERS_H
+
+#endif //MALLOC_WORLD_MONSTERS_H

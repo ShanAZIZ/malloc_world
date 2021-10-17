db933b4e50647b22e17bc93a9c5fc672c770ec60
Shan <70942213+ShanAZIZ@users.noreply.github.com>
2021-10-17 17:40:17 +0200

    [Fix] Rectification du header player.h

--- src/player/player.h
+++ src/player/player.h
@@ -1,33 +1,21 @@
-#ifndef PLAYER
-#define PLAYER
+#ifndef MALLOC_WORLD_PLAYER_H
+#define MALLOC_WORLD_PLAYER_H
 #endif
-
-#define INVENTORY_TOTAL 10
 
 /**
  * Struct Player
- * TODO: Le joueur ne pourra avoir que 3 armes
- * TODO: Le joueur ne pourra avoir que 1 armure
- * TODO: Les ressources du craft peuvent s'empiler
  */
-typedef struct Player{
+typedef struct Player {
     int xp;
     int level;
-    int maxHp;
-    int currentHp;
-    int inventory[INVENTORY_TOTAL]; // Sera au definitif un array de struct "item"
-}player;
+    int max_hp;
+    int current_hp;
+    inventory inventory;
+} player;
 
 /**
  * Initialise un joueur de debut de partie
  * @return
  */
 player* initPlayer();
-
-/**
- * Ajoute un objet/arme/ressource dans l'inventaire d'un joueur
- * @param stuff_id
- * @param player
- */
-void appendToInventory(int stuff_id, player* player);
 

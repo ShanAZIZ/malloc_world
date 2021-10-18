b7242dd45875d30d440e0a0700b3afe79ffe84aa
Shan <70942213+ShanAZIZ@users.noreply.github.com>
2021-10-18 22:41:25 +0200

    [Fix] Suppression de max_hp du joueur

--- src/player/player.c
+++ src/player/player.c
@@ -2,11 +2,9 @@
 
 player * initPlayer(item** item_list){
     player * mainPlayer = malloc(sizeof(player));
-    mainPlayer->max_hp = 100;
     mainPlayer->current_hp = 100;
     mainPlayer->level = 1;
-    mainPlayer->xp=0;
+    mainPlayer->current_xp=0;
     mainPlayer->inventory = initPlayerInventory(item_list);
     return mainPlayer;
 }
-

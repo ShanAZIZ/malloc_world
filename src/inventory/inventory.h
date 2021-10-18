cc3f436d13e04b9f2cf2a4ad79e111d1277986fe
Shan <70942213+ShanAZIZ@users.noreply.github.com>
2021-10-18 22:06:25 +0200

    [Feature] Mise en place de inventory avec les fonctions append

---
+++ src/inventory/inventory.h
@@ -0,0 +1,26 @@
+#ifndef MALLOC_WORLD_INVENTORY_H
+#define MALLOC_WORLD_INVENTORY_H
+#endif
+
+#define INVENTORY_SIZE 10
+
+/**
+ * TODO: Les ressources du craft peuvent s'empiler
+ */
+typedef struct Inventory {
+    item** inventory_content;
+    int max_weapon;
+    int max_tool;
+    int max_armor;
+    int current_weapon;
+    int current_tool;
+    int current_armor;
+} inventory;
+
+inventory* initPlayerInventory(item** item_list);
+void appendItemToInventoryWhereEmpty(item* item, inventory* inventory);
+void appendNewItemToInventory(item* item, inventory* player_inventory);
+int canAppendArmor(inventory* player_inventory);
+int canAppendTool(inventory* player_inventory);
+int canAppendWeapon(inventory* player_inventory);
+

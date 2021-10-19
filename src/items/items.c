45d88f9768b11663619072e41c3964d944fe73c6
Audrey <audrey.divito@protonmail.com>
2021-10-19 20:38:00 +0200

    Revert "Revert "Merge remote-tracking branch 'origin/items' into pnj""
    This reverts commit d7ed4e251e1cb8b72dd650b5e4241af174d3add5.

---
+++ src/items/items.c
@@ -0,0 +1,60 @@
+/**
+ * FILENAME: items.c
+ * Made by: GROUPE 5 - AL2
+ * Projet: Malloc-World
+ * Date de création: 15/10/2021
+ * Dernière modification : 15/10/2021
+ * Par: DAILLY Armand
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "items.h"
+
+item** createItemList(){
+    int count = 1;
+    item **itemList = malloc(sizeof(item*) * 35);
+    itemList[0] = malloc(sizeof(item));
+    itemList[0] = NULL;
+
+    char* names[34] = {"Epee en bois", "Pioche en bois", "Serpe en bois", "Hache en bois", "Sapin", "Pierre",
+                          "Herbe", "Epee en pierre", "Lance en pierre", "Marteau en pierre", "Plastron en pierre",
+                          "Pioche en pierre", "Serpe en pierre", "Hache en pierre", "Potion de vie I", "Hetre", "Fer",
+                          "Lavande", "Epee en fer", "Lance en fer", "Marteau en fer", "Plastron en fer",
+                          "Pioche en fer", "Serpe en fer", "Hache en fer", "Potion de vie II", "Chene", "Diamant",
+                          "Chanvre", "Epee en diamant", "Lance en diamant", "Marteau en diamant", "Plastron en diamant",
+                          "Potion de vie III"};
+
+    char* types[34] = {"Arme", "Outil", "Outil", "Outil", "Ressource de craft", "Ressource de craft",
+                          "Ressource de craft", "Arme", "Arme", "Arme", "Armure",
+                          "Outil", "Outil", "Outil", "Soin", "Ressource de craft", "Ressource de craft",
+                          "Ressource de craft", "Arme", "Arme", "Arme", "Armure",
+                          "Outil", "Outil", "Outil", "Soin", "Ressource de craft", "Ressource de craft",
+                          "Ressource de craft", "Arme", "Arme", "Arme", "Armure",
+                          "Soin"};
+
+    unsigned short damage[34] = {1, 0, 0, 0, 0, 0, 0, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 5, 7, 10, 0, 0, 0, 0, 0, 0,
+                      0, 0, 10, 15, 20, 0, 0};
+    unsigned short durability[34] = {10, 10, 10, 10, 1, 1, 1, 10, 8, 5, 1, 10, 10, 10, 1, 1, 1, 1, 10, 8, 5, 1, 10, 10, 10, 1, 1,
+                          1, 1, 10, 8, 5, 1, 1};
+    unsigned short protection[34] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                          0, 40, 0};
+
+    for (int i = 1; i < 35; i++) {
+        count += 1;
+        itemList[i] = malloc(sizeof(item));
+        *itemList[i] = (item) {i, names[i - 1], types[i - 1], damage[i - 1], durability[i - 1], 1, protection[i - 1]};
+    }
+    for (int i = 1; i < count; i++) {
+        printf("%p | %d | %s | %s | %d | %d | %d | %d\n", itemList[i], itemList[i]->value, itemList[i]->name,
+               itemList[i]->type, itemList[i]->damage, itemList[i]->durability, itemList[i]->quantity,
+               itemList[i]->protection);
+    }
+
+    return itemList;
+}
+
+item* getOneItem(item** itemList, int id){
+    return itemList[id];
+}

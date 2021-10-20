1668c61a3dfcb37feaee17e5b0ccd75bc27cac62
Armand <armanddailly@gmail.com>
2021-10-21 00:13:15 +0200

    [Rework] Generation of map
    - Reduced length of functions that were above 30 lines.
    - Add min ressources and min monsters.
    - Create function placeWalls

--- src/map/map.c
+++ src/map/map.c
@@ -37,119 +37,142 @@ int** initMap(int width, int height, int zone){
     return map;
 }
 
-//function to place grass
-int** placeGrass(int** map, int posX, int posY, int zone, int* cpt_grass){
-    if(zone == 1){
-        map[posX][posY] = 3;
-    }else if(zone == 2){
-        map[posX][posY] = 6;
-    }else{
-        map[posX][posY] = 9;
+//function that place walls
+int** placeWalls(int** map, int width, int height){
+    int randomPosX;
+    int randomPosY;
+    int cpt_walls;
+    cpt_walls = 0;
+    while(cpt_walls <= (3 + 0.1 * (width * height))){
+        randomPosX = rand() % width;
+        randomPosY = rand() % height;
+        if(map[randomPosX][randomPosY] == 0){
+            map[randomPosX][randomPosY] = -1;
+            cpt_walls += 1;
+        }
     }
-    *cpt_grass += 1;
+    return map;
+}
+
+//function to place grass
+int** placeGrass(int** map, int width, int height, int zone){
+    int randomPosX;
+    int randomPosY;
+    int cpt_grass;
+    cpt_grass = 0;
+    while(cpt_grass <= (3 + 0.1 * (width * height))){
+        randomPosX = rand() % width;
+        randomPosY = rand() % height;
+        if(map[randomPosX][randomPosY] == 0){
+            if(zone == 1){
+                map[randomPosX][randomPosY] = 3;
+            }else if(zone == 2){
+                map[randomPosX][randomPosY] = 6;
+            }else{
+                map[randomPosX][randomPosY] = 9;
+            }
+            cpt_grass += 1;
+        }
+    }
     return map;
 }
 
 //function to place rock
-int** placeRock(int** map, int posX, int posY, int zone, int* cpt_rock){
-    if(zone == 1){
-        map[posX][posY] = 4;
-    }else if(zone == 2){
-        map[posX][posY] = 7;
-    }else{
-        map[posX][posY] = 10;
+int** placeRock(int** map, int width, int height, int zone){
+    int randomPosX;
+    int randomPosY;
+    int cpt_rock;
+    cpt_rock = 0;
+    while(cpt_rock <= (3 + 0.1 * (width * height))){
+        randomPosX = rand() % width;
+        randomPosY = rand() % height;
+        if(map[randomPosX][randomPosY] == 0){
+            if(zone == 1){
+                map[randomPosX][randomPosY] = 4;
+            }else if(zone == 2){
+                map[randomPosX][randomPosY] = 7;
+            }else{
+                map[randomPosX][randomPosY] = 10;
+            }
+            cpt_rock += 1;
+        }
     }
-    *cpt_rock += 1;
     return map;
 }
 
 //function to place wood
-int** placeWood(int** map, int posX, int posY, int zone, int* cpt_wood){
-    if(zone == 1){
-        map[posX][posY] = 5;
-    }else if(zone == 2){
-        map[posX][posY] = 8;
-    }else{
-        map[posX][posY] = 11;
+int** placeWood(int** map, int width, int height, int zone){
+    int randomPosX;
+    int randomPosY;
+    int cpt_wood;
+    cpt_wood = 0;
+    while(cpt_wood <= (3 + 0.1 * (width * height))){
+        randomPosX = rand() % width;
+        randomPosY = rand() % height;
+        if(map[randomPosX][randomPosY] == 0){
+            if(zone == 1){
+                map[randomPosX][randomPosY] = 5;
+            }else if(zone == 2){
+                map[randomPosX][randomPosY] = 8;
+            }else{
+                map[randomPosX][randomPosY] = 11;
+            }
+            cpt_wood += 1;
+        }
     }
-    *cpt_wood += 1;
-    return map;
-}
-
-//function to place monster
-int** placeMonster(int** map, int posX, int posY, int zone, int* cpt_monster){
-    switch (rand() % 2 + 1) {
-        case 1:
-            map[posX][posY] = rand() % 85 + 12;
-            break;
-        default:
-            break;
-    }
-    *cpt_monster += 1;
     return map;
 }
 
 //function to place the NPC
-int** placeNPC(int** map, int width){
+int** placeNPC(int** map, int width, int height){
+    int randomPosX;
+    int randomPosY;
     do{
-        int randomPos = rand() % (width - 1);
-        if(map[randomPos][randomPos] != 0){
+        randomPosX = rand() % width;
+        randomPosY = rand() % height;
+        if(map[randomPosX][randomPosY] != 0){
             continue;
         }else{
-            map[randomPos][randomPos] = 2;
+            map[randomPosX][randomPosY] = 2;
         }
         break;
     }while(1);
     return map;
 }
 
-//function
-int** placePortal(int** map, int width, int zone){
-    int randomPos;
-    int finished = 0;
-    do{
-        randomPos = rand() % (width - 1);
-        switch (zone) {
-            case 1:
-                if(map[randomPos][randomPos] != 0){
+//function that places the portals
+int** placePortal(int** map, int width, int height, int zone){
+    int randomPosX;
+    int randomPosY;
+    while(1){
+        randomPosX = rand() % width;
+        randomPosY = rand() % height;
+        if(map[randomPosX][randomPosY] != 0){
+            continue;
+        }else if(zone == 1){
+            map[randomPosX][randomPosY] = -2;
+        }else if(zone == 2){
+            map[randomPosX][randomPosY] = -2;
+            while(1){
+                randomPosX = rand() % width;
+                randomPosY = rand() % height;
+                if(map[randomPosX][randomPosY] != 0){
                     continue;
                 }else{
-                    map[randomPos][randomPos] = -2;
-                    finished = 1;
+                    map[randomPosX][randomPosY] = -3;
                 }
                 break;
-            case 2:
-                if(map[randomPos][randomPos] != 0){
-                    continue;
-                }else{
-                    map[randomPos][randomPos] = -2;
-                    do{
-                        randomPos = rand() % (width - 1);
-                        if(map[randomPos][randomPos] != 0){
-                            continue;
-                        }else{
-                            map[randomPos][randomPos] = -3;
-                            finished = 1;
-                            break;
-                        }
-                    }while(1);
-                }
-                break;
-            default:
-                if(map[randomPos][randomPos] != 0){
-                    continue;
-                }else{
-                    map[randomPos][randomPos] = -3;
-                    finished = 1;
-                }
-                break;
+            }
+        }else if(zone == 3){
+            map[randomPosX][randomPosY] = -3;
         }
-    }while(finished != 1);
+        break;
+    }
     return map;
 }
 
 //function to place boss in zone 3
-int** placeBoss(int** map, int width){
+int** placeBoss(int** map, int width, int height){
     do{
         int randomPos = rand() % (width - 1);
         if(map[randomPos][randomPos] != 0){
@@ -162,57 +185,35 @@ int** placeBoss(int** map, int width){
     return map;
 }
 
-//function that fill the map using all place functions except placeBoss
-int** fillMap(int** map, int width, int height, int zone){
-    int* cpt_grass = malloc(sizeof(int));
-    *cpt_grass = 0;
-    int* cpt_rock = malloc(sizeof(int));
-    *cpt_rock = 0;
-    int* cpt_wood = malloc(sizeof(int));
-    *cpt_wood = 0;
-    int* cpt_monster = malloc(sizeof(int));
-    *cpt_monster = 0;
-    for(int i = 0; i < width; i++) {
-        for(int j = 0; j < height; j++) {
-            if(map[i][j] == 0){
-                switch (((width + 1) == 6) ? (rand() % 6 + 1) : (rand() % width + 1)) {
-                    case 1:
-                        if(*cpt_grass <= (3 + 0.1 * (width * height))){
-                            map = placeGrass(map, i, j, zone, cpt_grass);
-                        }
-                        break;
-                    case 2:
-                        if(*cpt_rock <= (3 + 0.1 * (width * height))){
-                            map = placeRock(map, i, j, zone, cpt_rock);
-                        }
-                        break;
-                    case 3:
-                        if(*cpt_wood <= (3 + 0.1 * (width * height))){
-                            map = placeWood(map, i, j, zone, cpt_wood);
-                        }
-                        break;
-                    case 4:
-                        if(*cpt_monster <= (10 + 0.05 * (width * height))){
-                            map = placeMonster(map, i, j, zone, cpt_monster);
-                        }
-                        break;
-                    case 5:
-                        if(*cpt_monster <= (10 + 0.05 * (width * height))){
-                            map = placeMonster(map, i, j, zone, cpt_monster);
-                        }
-                        break;
-                    case 6:
-                        map[i][j] = -1;
-                        break;
-                    default:
-                        break;
-                }
-            }
+//function that places monsters
+int** placeMonsters(int** map, int width, int height, int zone){
+    int randomPosX;
+    int randomPosY;
+    int cpt_monster;
+    cpt_monster = 0;
+    while(cpt_monster <= (10 + 0.05 * (width * height))){
+        randomPosX = rand() % width;
+        randomPosY = rand() % height;
+        if(map[randomPosX][randomPosY] == 0){
+            map[randomPosX][randomPosY] = rand() % 85 + 12;
+            cpt_monster += 1;
         }
     }
-    map = placeNPC(map, width);
-    map = placePortal(map, width, zone);
     return map;
 }
 
+//function that fill the map by calling all the "place" functions
+int** fillMap(int** map, int width, int height, int zone){
+    if(zone == 3){
+        map = placeBoss(map, width, height);
+    }
+    map = placeNPC(map, width, height);
+    map = placePortal(map, width, height, zone);
+    map = placeMonsters(map, width, height, zone);
+    map = placeWood(map, width, height, zone);
+    map = placeRock(map, width, height, zone);
+    map = placeGrass(map, width, height, zone);
+    map = placeWalls(map, width, height);
+    return map;
+}
 

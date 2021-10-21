376e83b89d20008bed941a53229534cb54b9f2ee
Armand <armanddailly@gmail.com>
2021-10-22 01:06:45 +0200

    [Feature + Rework] Player and inventory
     - Add function getInventory to player
     - Changed function appendItemToInventoryWhereEmpty from comparing if inventory content was equal to NULL to comparing if value equal 0
     - Add handling of crafting ressources, stack them if stack not full and create a new stack if the other(s) one already full

--- src/inventory/inventory.c
+++ src/inventory/inventory.c
@@ -1,10 +1,18 @@
+/**
+ * FILENAME: inventory.c
+ * Made by: GROUPE 5 - AL2
+ * Projet: Malloc-World
+ * Date de création: 17/10/2021
+ * Dernière modification : 22/10/2021
+ * Par: DAILLY Armand
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "inventory.h"
 
-inventory* initPlayerInventory(item** item_list){
-    inventory* player_inventory = malloc(sizeof(inventory));
+inventory *initPlayerInventory(item **itemList) {
+    inventory *player_inventory = malloc(sizeof(inventory));
     player_inventory->max_armor = 1;
     player_inventory->max_tool = 6;
     player_inventory->max_weapon = 3;
@@ -12,78 +20,83 @@ inventory* initPlayerInventory(item** item_list){
     player_inventory->current_weapon = 1;
     player_inventory->current_tool = 3;
     for (int i = 0; i < INVENTORY_SIZE; i++) {
-        player_inventory->inventory_content[i] = malloc(sizeof(item*));
+        player_inventory->inventory_content[i] = malloc(sizeof(item));
+        *(player_inventory->inventory_content[i]) = (item){0, "", "", 0, 0, 0, 0};
     }
-    player_inventory->inventory_content[0] = item_list[1];
-    player_inventory->inventory_content[1] = item_list[2];
-    player_inventory->inventory_content[2] = item_list[3];
-    player_inventory->inventory_content[3] = item_list[4];
+    appendItemToInventoryWhereEmpty(itemList, 1, player_inventory);
+    appendItemToInventoryWhereEmpty(itemList, 2, player_inventory);
+    appendItemToInventoryWhereEmpty(itemList, 3, player_inventory);
+    appendItemToInventoryWhereEmpty(itemList, 4, player_inventory);
     return player_inventory;
-};
+}
 
-void appendItemToInventoryWhereEmpty(item* item, inventory* inventory){
+void appendItemToInventoryWhereEmpty(item** itemList, int itemId, inventory *inventory) {
+    item* item = getOneItem(itemList, itemId);
     for (int i = 0; i < INVENTORY_SIZE; i++) {
-        if(inventory->inventory_content[i] == NULL){
-            inventory->inventory_content[i] = item;
+        if (inventory->inventory_content[i]->value == 0) {
+            inventory->inventory_content[i]->value = item->value;
+            inventory->inventory_content[i]->name = item->name;
+            inventory->inventory_content[i]->type = item->type;
+            inventory->inventory_content[i]->damage = item->damage;
+            inventory->inventory_content[i]->durability = item->durability;
+            inventory->inventory_content[i]->quantity = item->quantity;
+            inventory->inventory_content[i]->protection = item->protection;
+
             break;
         }
     }
 }
 
-void appendNewItemToInventory(item* item, inventory* player_inventory){
-    if(strcmp(item->type, "Arme") == 0){
-        if(canAppendWeapon(player_inventory)){
-            appendItemToInventoryWhereEmpty(item, player_inventory);
+void appendNewItemToInventory(item** itemList, item* item, inventory *player_inventory) {
+    if (strcmp(item->type, "Arme") == 0) {
+        if (canAppendWeapon(player_inventory)) {
+            appendItemToInventoryWhereEmpty(itemList, item->value, player_inventory);
         }
     }
-    if(strcmp(item->type, "Outil") == 0){
-        if(canAppendTool(player_inventory)){
-            appendItemToInventoryWhereEmpty(item, player_inventory);
+    if (strcmp(item->type, "Outil") == 0) {
+        if (canAppendTool(player_inventory)) {
+            appendItemToInventoryWhereEmpty(itemList, item->value, player_inventory);
         }
     }
-    if(strcmp(item->type, "Ressource de craft") == 0){
-
+    if (strcmp(item->type, "Soin") == 0) {
+        appendItemToInventoryWhereEmpty(itemList, item->value, player_inventory);
     }
-    if(strcmp(item->type, "Soin") == 0){
-        appendItemToInventoryWhereEmpty(item, player_inventory);
-    }
-    if(strcmp(item->type, "Armure") == 0){
-        if(canAppendArmor(player_inventory)){
-            appendItemToInventoryWhereEmpty(item, player_inventory);
+    if (strcmp(item->type, "Armure") == 0) {
+        if (canAppendArmor(player_inventory)) {
+            appendItemToInventoryWhereEmpty(itemList, item->value, player_inventory);
         }
     }
 }
 
-//void appendRessourceDuCraft(item* item, inventory* player_inventory){
-//    int already_stacked = 0;
-//    for(int i = 0; i < INVENTORY_SIZE ; i++) {
-//        if(item->value == player_inventory->inventory_content[i]->value) {
-//            if (player_inventory->inventory_content[i]->quantity < 20) {
-//                player_inventory->inventory_content[i]->quantity += 1;
-//                already_stacked = 1;
-//            }
-//        }
-//    }
-//    if(already_stacked == 0) {
-//    }
-//}
+void appendRessourceDeCraft(item** itemList, int itemId, inventory* player_inventory){
+    item* item = getOneItem(itemList, itemId);
+    for(int i = 0; i < INVENTORY_SIZE ; i++) {
+        if(item->value == player_inventory->inventory_content[i]->value && player_inventory->inventory_content[i]->quantity < 20) {
+            player_inventory->inventory_content[i]->quantity += 1;
+            break;
+        }else if(player_inventory->inventory_content[i]->value == 0){
+            appendItemToInventoryWhereEmpty(itemList, itemId, player_inventory);
+            break;
+        }
+    }
+}
 
-int canAppendArmor(inventory* player_inventory){
-    if(player_inventory->max_armor > player_inventory->current_armor){
+int canAppendArmor(inventory *player_inventory) {
+    if (player_inventory->max_armor > player_inventory->current_armor) {
         return 1;
     }
     return 0;
 }
 
-int canAppendTool(inventory* player_inventory){
-    if(player_inventory->max_tool > player_inventory->current_tool){
+int canAppendTool(inventory *player_inventory) {
+    if (player_inventory->max_tool > player_inventory->current_tool) {
         return 1;
     }
     return 0;
 }
 
-int canAppendWeapon(inventory* player_inventory){
-    if(player_inventory->max_weapon > player_inventory->current_weapon){
+int canAppendWeapon(inventory *player_inventory) {
+    if (player_inventory->max_weapon > player_inventory->current_weapon) {
         return 1;
     }
     return 0;
--- src/main.c
+++ src/main.c
@@ -3,19 +3,21 @@
  * Made by: GROUPE 5 - AL2 
  * Projet: Malloc-World
  * Date de création: 09/10/2021
- * Dernière modification : 09/10/2021
- * Par: AZIZ Shana
+ * Dernière modification : 22/10/2021
+ * Par: DAILLY Armand
  */
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<player.h>
+#include "player/player.h"
 
 int main(int argc, char const *argv[])
 {
     printf("bonjour\n");
     item** item_list = createItemList();
     player* my_player = initPlayer(item_list);
-    printf("%s", my_player->inventory->inventory_content[0]->type);
+    appendRessourceDeCraft(item_list, 6, my_player->inventory);
+    my_player->inventory->inventory_content[4]->quantity = 20;
+    appendRessourceDeCraft(item_list, 6, my_player->inventory);
     return 0;
 }

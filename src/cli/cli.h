f8937abab992cd40a2042559e5c963793de2ac97
Shan <70942213+ShanAZIZ@users.noreply.github.com>
2021-10-22 11:55:52 +0200

    [Feature] fonction title

---
+++ src/cli/cli.h
@@ -0,0 +1,26 @@
+/**
+ * FILENAME: main.c
+ * Made by: GROUPE 5 - AL2
+ * Projet: Malloc-World
+ * Date de création: 09/10/2021
+ * Dernière modification : 09/10/2021
+ * Par: AZIZ Shana
+ */
+
+#ifndef MALLOC_WORLD_CLI_H
+#define MALLOC_WORLD_CLI_H
+
+#endif //MALLOC_WORLD_CLI_H
+
+void title();
+
+int mainMenu();
+
+int newGame();
+
+int loadGame();
+
+int interactionPNJ();
+
+int tour();
+

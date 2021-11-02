177e82f0a36abd781aa053534f8ba9f867fd4610
Shan <70942213+ShanAZIZ@users.noreply.github.com>
2021-11-02 18:40:01 +0100

    [Feature] Save of the map

---
+++ src/save/save.h
@@ -0,0 +1,21 @@
+/**
+ * FILENAME: save.h
+ * Made by: GROUPE 5 - AL2
+ * Projet: Malloc-World
+ * Date de création: 27/10/2021
+ * Dernière modification : 27/10/2021
+ * Par: AZIZ Shana
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "map.h"
+
+#ifndef MALLOC_WORLD_SAVE_H
+#define MALLOC_WORLD_SAVE_H
+
+#endif
+
+void saveZone(int**, int, int);
+void saveMap(int**, int**, int**, int, int);

d1be9ee84af100dc9b83606068928965db72e2fd
shan <shan.azizh@gmail.com>
2021-11-07 17:02:12 +0100

    [Fix] fix malloc of inventory_content, delete of unused code

--- src/inventory/inventory.h
+++ src/inventory/inventory.h
@@ -15,19 +15,8 @@
 
 typedef struct Inventory {
     item** inventory_content;
-    int max_weapon;
-    int max_tool;
-    int max_armor;
-    int current_weapon;
-    int current_tool;
-    int current_armor;
 } inventory;
 
 inventory* initPlayerInventory(item** item_list);
 void appendItemToInventoryWhereEmpty(item** itemList, int itemId, inventory *inventory);
-void appendNewItemToInventory(item** itemList, item* item, inventory* player_inventory);
 void appendRessourceDeCraft(item** itemList, int itemId, inventory* player_inventory);
-int canAppendArmor(inventory* player_inventory);
-int canAppendTool(inventory* player_inventory);
-int canAppendWeapon(inventory* player_inventory);
-

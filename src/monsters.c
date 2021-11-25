c1daae57333225c53159ba5970ebb9a6aaaffe6b
shan <shan.azizh@gmail.com>
2021-11-25 23:44:55 +0100

    [Feature] Emmanuel Macron

--- src/monsters.c
+++ src/monsters.c
@@ -17,7 +17,7 @@ Monster **createMonsterList(void) {
         monsterList[i] = malloc(sizeof(Monster));
         monsterList[i] = NULL;
     }
-    char *names[10] = {"Sanglier", "Slime", "Assassin", "Petit ours brun", "Sananes", "Caribou", "Pomme de terre vivante", "Flan", "Ancien dragon", "Jesus"};
+    char *names[10] = {"Sanglier", "Slime", "Emmanuel Macron", "Petit ours brun", "Sananes", "Caribou", "Pomme de terre vivante", "Flan", "Ancien dragon", "Jesus"};
     unsigned short hp[10] = {10, 5, 12, 16, 70, 25, 7, 2, 60, 80};
     unsigned short att[10] = {5, 1, 4, 8, 60, 15, 3, 3, 35, 50};
     unsigned short def[10] = {1, 0, 3, 6, 40, 15, 1, 2, 40, 20};
